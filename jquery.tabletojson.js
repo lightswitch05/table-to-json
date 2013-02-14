2baa8f50a691945a203a1c0d7376cfb994b947e9
Daniel <dswhite21@gmail.com>
2013-02-14 13:07:00 -0500

    Added tests and fixed a few bugs found by tests

--- jquery.tabletojson.js
+++ jquery.tabletojson.js
@@ -2,7 +2,7 @@
   Table To JSON jQuery Plugin
   by Daniel White - developerdan.com
 
-  Version: 0.2.2
+  Version: 0.3.0
 
   Copyright (c) 2013 Daniel White. Released under the MIT License.
 
@@ -22,9 +22,10 @@
 
     // Gather headings
     var headings = new Array();
-    this.find("thead tr th").each(function(colIndex, col) {
+    this.find("tr > th").each(function(colIndex, col) {
       if($.inArray(colIndex, opts.ignoreColNum) == -1) {
-        if($(col).data("column-name") != undefined || $(col).data("column-name") != null) {
+        if($(col).data("column-name") != undefined && $(col).data("column-name") != null) {
+          console.log("USING COLUMNS-VALUE" + $(col).data("column-name"));
           headings[colIndex] = $(col).data("column-name");
         } else {
           headings[colIndex] = $.trim($(col).text());
@@ -34,24 +35,27 @@
       }
     });
 
-    var visible = ":visible";
-    if(!opts.ignoreHiddenRows){
-      visible = "";
-    }
-
     // Gather values
     var values = new Array();
-    this.find("tbody tr").filter(visible).each(function(rowIndex, row) {
-      values[rowIndex] = {};
-      $(row).children("td").each(function(colIndex, col) {
-        if( headings[colIndex] != null ){
-          if($(col).data("cell-value") != undefined || $(col).data("cell-value") != null) {
-            values[rowIndex][ headings[colIndex] ] = $(col).data("cell-value");
-          } else {
-            values[rowIndex][ headings[colIndex] ] = $.trim($(col).text());
+    this.children("tbody").children("tr:has(td)")
+    .filter(function(index){
+      return $(this).children("th").length == 0
+    }).each(function(rowIndex, row) {
+    //this.find("this > tr:has(td):not(:has(th))").each(function(rowIndex, row) {
+      if( $(row).is(':visible') || !opts.ignoreHiddenRows ) {
+        values[rowIndex] = {};
+        $(row).children("td").each(function(colIndex, col) {
+          if( headings[colIndex] != undefined && headings[colIndex] != null){
+            if($(col).data("cell-value") != undefined && $(col).data("cell-value") != null) {
+              console.log("USING CELL-VALUE" + $(col).data("cell-value"));
+              console.log( col );
+              values[rowIndex][ headings[colIndex] ] = $(col).data("cell-value");
+            } else {
+              values[rowIndex][ headings[colIndex] ] = $.trim($(col).text());
+            }
           }
-        }
-      });
+        });
+      }
     });
     return values;
   };
---
+++ test/specs/content.test.js
@@ -0,0 +1,122 @@
+module("content");
+
+/* Ignore nested tables in cells */
+test("ignore nested tables in cells", function() {
+  $("#qunit-fixture").html(
+      "<table id='test-table'>" +
+        "<tr>" +
+          "<th>First Name</th>" +
+          "<th>Last Name</th>" +
+          "<th>Points</th>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>Jill</td>" +
+          "<td>Smith</td>" +
+          "<td>50</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>Eve <table><tr><td>1</td></tr><tr><td>2</td></tr></table> </td>" +
+          "<td>Jackson</td>" +
+          "<td>94</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>John</td>" +
+          "<td>Doe</td>" +
+          "<td>80</td>" +
+        "</tr>" +
+      "</table>"
+    );
+
+
+  expect(1);
+  var table = $("#test-table").tableToJSON();
+  var expected = [{"First Name":"Jill", "Last Name":"Smith", "Points":"50"},
+                  {"First Name":"Eve 12", "Last Name":"Jackson", "Points":"94"},
+                  {"First Name":"John", "Last Name":"Doe", "Points":"80"}]
+  deepEqual(table, expected);
+});
+
+/* Ignore nested tables in headings */
+test("ignore nested tables in headings", function() {
+  $("#qunit-fixture").html(
+      "<table id='test-table'>" +
+        "<tr>" +
+          "<th>First Name</th>" +
+          "<th>Last Name <table><tr><td>1</td></tr><tr><td>2</td></tr></table> </th>" +
+          "<th>Points</th>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>Jill</td>" +
+          "<td>Smith</td>" +
+          "<td>50</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>Eve</td>" +
+          "<td>Jackson</td>" +
+          "<td>94</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>John</td>" +
+          "<td>Doe</td>" +
+          "<td>80</td>" +
+        "</tr>" +
+      "</table>"
+    );
+
+
+  expect(1);
+  var table = $("#test-table").tableToJSON();
+  var expected = [{"First Name":"Jill", "Last Name 12":"Smith", "Points":"50"},
+                  {"First Name":"Eve", "Last Name 12":"Jackson", "Points":"94"},
+                  {"First Name":"John", "Last Name 12":"Doe", "Points":"80"}]
+  deepEqual(table, expected);
+});
+
+/* Links are retrieved as values */
+test("links are just values", function() {
+  $("#qunit-fixture").html(
+      "<table id='test-table'>" +
+        "<tr>" +
+          "<th>First Name</th>" +
+          "<th>Last Name</th>" +
+          "<th>Points</th>" +
+        "</tr>" +
+        "<tr>" +
+          "<td><a href='#'>Jill</a></td>" +
+          "<td><a href='#'>Smith</a></td>" +
+          "<td><a href='#'>50</a></td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>Eve</td>" +
+          "<td>Jackson</td>" +
+          "<td>94</td>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>John</td>" +
+          "<td>Doe</td>" +
+          "<td>80</td>" +
+        "</tr>" +
+      "</table>"
+    );
+
+
+  expect(1);
+  var table = $("#test-table").tableToJSON();
+  var expected = [{"First Name":"Jill", "Last Name":"Smith", "Points":"50"},
+                  {"First Name":"Eve", "Last Name":"Jackson", "Points":"94"},
+                  {"First Name":"John", "Last Name":"Doe", "Points":"80"}]
+  deepEqual(table, expected);
+});
+
+/* A complex table */
+test("complex table", function() {
+  $("#qunit-fixture").html(
+      '<table id="test-table" class="table table-striped table-hover table-condensed" data-role="rate_table"> <thead> <tr> <th data-column-name="id"><input type="checkbox" data-role="checkall" data-check-name="row_ids[]"></th> <th data-column-name="State">Status</th> <th data-column-name="BCS">Location</th> <th data-column-name="ZONE">Zone</th> <th data-column-name="TIER">Tier</th> <th data-column-name="INSTALL_CALC">Install Calc</th> <th data-column-name="INSTALL_R1">Install Rate</th> </tr> </thead> <tbody> <tr> <td data-cell-value="oaWd4cs2JHY660pG"><input id="oaWd4cs2JHY660pG" name="row_ids[]" type="checkbox" value="oaWd4cs2JHY660pG"> <i class="icon-arrow-down hover" data-title="Copy row" data-role="copy-row" data-original-title=""></i> <i class="icon-remove hover" data-title="Delete row" data-role="delete-row" data-original-title=""></i> <i class="icon-warning-sign hover" data-role="ok-modal" data-modal-name="row-errors-modal" data-title="Display rate table errors" data-original-title=""></i> <div class="hidden" data-role="row-errors-modal" title="Rate Table Errors"> <p> The specified value () is invalid for the field (ACCSIN_TYP). </p> <p> The specified value () is invalid for the field (ZONE). </p> <p> The specified value () is invalid for the field (TIERIND). </p> <p> The specified value () is invalid for the field (CALCTYPE). </p> <p> The required decimal value is not valid.  The value must be between (-9999999999.999999) and (9999999999.999999). </p> </div>   <i data-title="Row Audit Trail" data-modal-name="audit-modal" data-role="ok-modal" class="icon-tags hover" data-original-title=""></i> <div class="hidden" data-role="audit-modal" title="Audit Trail"> <table class="table table-striped table-hover table-condensed"> <thead> <tr> <th>Date</th> <th>User ID</th> <th>From</th> <th>To</th> <th>Action</th> </tr> </thead> <tbody><tr> <td> 2013-02-12 09:26:21 </td> <td> tapadmin </td> <td>  </td> <td> INCOMPLETE </td> <td> Rate Table Detail Creation </td> </tr><tr> <td> 2013-02-12 09:26:21 </td> <td> tapadmin </td> <td> INCOMPLETE </td> <td> INCOMPLETE </td> <td> Rate table change </td> </tr><tr> <td> 2013-02-13 07:37:26 </td> <td> tapadmin </td> <td> INCOMPLETE </td> <td> INCOMPLETE </td> <td> Rate table change </td> </tr><tr> <td> 2013-02-14 09:04:40 </td> <td> tapadmin </td> <td> INCOMPLETE </td> <td> INCOMPLETE </td> <td> Rate table change </td> </tr><tr> <td> 2013-02-14 09:05:33 </td> <td> tapadmin </td> <td> INCOMPLETE </td> <td> INCOMPLETE </td> <td> Rate table change </td> </tr><tr> <td> 2013-02-14 09:49:55 </td> <td> tapadmin </td> <td> INCOMPLETE </td> <td> INCOMPLETE </td> <td> Rate table change </td> </tr><tr> <td> 2013-02-14 09:50:11 </td> <td> tapadmin </td> <td> INCOMPLETE </td> <td> INCOMPLETE </td> <td> Rate table change </td> </tr><tr> <td> 2013-02-14 09:50:11 </td> <td> tapadmin </td> <td>  </td> <td> INCOMPLETE </td> <td> Rate row created </td> </tr></tbody></table> </div> </td> <td data-type="row-status">INCOMPLETE</td> <td> <a data-type="text" data-group-code="ACCSIN_TYP" class="editable editable-click editable-unsaved" data-original-title="">location</a> </td> <td> <a data-type="select" data-group-code="ZONE" class="editable editable-click editable-unsaved" data-original-title="">1</a> </td> <td> <a data-type="select" data-group-code="TIERIND" class="editable editable-click editable-unsaved" data-original-title="">QTY</a> </td> <td> <a data-type="text" data-group-code="" class="editable editable-click editable-unsaved" data-original-title="">B</a> </td> <td> <a data-type="text" data-group-code="" class="editable editable-click editable-unsaved" data-original-title="">9.5</a> </td> </tr> <tr style="display: none;"> <td data-cell-value=""><input id="" name="row_ids[]" type="checkbox" value="new_row"> <i class="icon-arrow-down hover" data-title="Copy row" data-role="copy-row" data-original-title=""></i> <i class="icon-remove hover" data-title="Delete row" data-role="delete-row" data-original-title=""></i> </td> <td data-type="row-status">INCOMPLETE</td> <td> <a data-type="text" data-group-code="ACCSIN_TYP" class="editable editable-click editable-empty">Empty</a> </td> <td> <a data-type="select" data-group-code="ZONE" class="editable editable-click editable-empty">Empty</a> </td> <td> <a data-type="select" data-group-code="TIERIND" class="editable editable-click editable-empty">Empty</a> </td> <td> <a data-type="text" data-group-code="" class="editable editable-click editable-empty">Empty</a> </td> <td> <a data-type="text" data-group-code="" class="editable editable-click editable-empty">Empty</a> </td> </tr> </tbody> </table>'
+    );
+
+
+  expect(1);
+  var table = $("#test-table").tableToJSON();
+  var expected = [{"id":"oaWd4cs2JHY660pG", "State":"INCOMPLETE", "BCS":"location", "ZONE":"1", "TIER":"QTY", "INSTALL_CALC":"B", "INSTALL_R1":"9.5"}]
+  deepEqual(table, expected);
+});
